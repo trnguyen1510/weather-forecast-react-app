7cf5727c47064e122db318a8cd45d189e7e2cefd
trnguyen1510 <trnguyen1510@gmail.com>
2021-02-20 13:46:56 -0800

    added github url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 
 export default function App() {
-  return(
-  <div className="App">Weather App</div>
+  return (
+    <div className="App">
+      Weather App
+      <footer> This project was coded by Tram Nguyen and is {" "}
+        <a href="https://github.com/trnguyen1510/weather-forecast-react-app" target="_blank">open-source on Github</a>
+
+      </footer>
+    </div>
   );
 }
