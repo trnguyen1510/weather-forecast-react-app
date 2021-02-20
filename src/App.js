d2c63f8028a3611b7eeffde07bb567a737c6bd73
trnguyen1510 <trnguyen1510@gmail.com>
2021-02-20 14:04:10 -0800

    added bootsrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer> This project was coded by Tram Nguyen and is {" "}
-        <a href="https://github.com/trnguyen1510/weather-forecast-react-app" target="_blank">open-source on Github</a>
+      <div className="container">
+        <h1>Weather App</h1> 
+        <button className="btn btn-primary">Search</button>
 
-      </footer>
+      <footer> This project was coded by Tram Nguyen and is {" "}
+          <a href="https://github.com/trnguyen1510/weather-forecast-react-app" target="_blank">open-source on Github</a>
+
+
+        </footer>
+      </div>
     </div>
   );
 }
