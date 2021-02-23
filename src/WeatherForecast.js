acbf4b20f91f6983731e5a7126f26562e5159828
trnguyen1510 <trnguyen1510@gmail.com>
2021-02-22 16:40:25 -0800

    switch http to https for forecast components

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,7 +16,7 @@ export default function WeatherForecast(props) {
     if (loaded && props.city === forecast.city.name) {
 
         return (<div className="WeatherForecast row">
-            {forecast.list.slice(0,6).map(function(forecastItem){
+            {forecast.list.slice(0, 6).map(function (forecastItem) {
                 return <WeatherForecastPreview data={forecastItem} />;
             })}
             {/*Another solution */}
@@ -31,7 +31,7 @@ export default function WeatherForecast(props) {
         );
     } else {
         let apiKey = "d80253f7bab7ebfb0d574948c8b564be";
-        let url = `http://api.openweathermap.org/data/2.5/forecast?q=
+        let url = `https://api.openweathermap.org/data/2.5/forecast?q=
     ${props.city}&appid=${apiKey}&units=metric`;
         axios.get(url).then(handleForecastResponse);
         return null;
